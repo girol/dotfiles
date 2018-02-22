6bfa116847a75a06da96397568c5ccfb1f4cc4de
André Luiz Girol <andregirol@gmail.com>
2018-02-22 18:33:30 -0300

    Added more steps to install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,14 @@
 echo "Installing .vimrc..."
 cp .vimrc ~/.vimrc
 
+echo "Preparing Pathogen..."
+echo "Creating directories"
+
+mkdir -p ~/.vim/autoload ~/.vim/bundle 
+
+echo "Downloading Pathogen and installing..."
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
 echo
 echo "vimrc installed into $HOME/.vimrc"
 echo

784c5d21c8976459ac3e7cbf3e3be5683dab1d5c
André Luiz Girol <andregirol@gmail.com>
2018-02-24 19:08:31 -0300

    Added nertdree-git-plugin and comments

--- install.sh
+++ install.sh
@@ -13,15 +13,23 @@ curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 
 echo "Preparing Plugins..."
 
+# Adds a beautiful status bar in the bottom
 echo "Installing airline"
 git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
 
+# Allows icons inside vim and other plugins
 echo "Installing vim-devicons"
 git clone https://github.com/ryanoasis/vim-devicons ~/.vim/bundle/vim-devicons
 
-
+# Nice file manager for vim
 echo "Installing NERDtree"
 git clone https://github.com/scrooloose/nerdtree.git ~/.vim/bundle/nerdtree
+
+echo "Installing git stuff"
+
+# Git status indicators for files in NERDtree
+echo "Installing nerdtree-git-plugin"
+git clone https://github.com/Xuyuanp/nerdtree-git-plugin.git ~/.vim/bundle/nerdtree-git-plugin
 
 echo
 echo "vimrc installed into $HOME/.vimrc"

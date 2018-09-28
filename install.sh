ae9b5f929cea220f567691333af9d2983eee97d9
André Luiz Girol <andregirol@gmail.com>
2018-09-28 18:30:57 -0300

    Agrupando lógica do script e autostart do vim

--- install.sh
+++ install.sh
@@ -16,13 +16,17 @@ echo "Installing .vimrc..."
 cp vimrc ~/.vimrc
 
 
+echo
+echo "vimrc installed into $HOME/.vimrc"
+echo
+
 echo "Preparing vim-plug..."
 
 echo "Downloading and installing..."
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo
-echo "vimrc installed into $HOME/.vimrc"
+echo "Plugging..."
 echo
 
+vim +PlugInstall +qall

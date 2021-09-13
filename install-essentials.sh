a438d9e4360adb0d323734ff59705e23f48f9427
André Luiz Girol <andregirol@gmail.com>
2021-09-13 11:59:14 -0300

    Renames base install script

--- install-essentials.sh
+++ install-essentials.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-echo -e "Installing base packages"
+# Installs the very essential packages to start working
+
+echo -e "Installing essential base packages"
 
 sudo apt install ack \
     apt-transport-https \

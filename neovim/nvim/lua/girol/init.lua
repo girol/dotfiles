38ab855cede08a538e080a0a748bd375789af7e1
André Luiz Girol <andregirol@gmail.com>
2024-05-16 18:42:59 -0300

    fix identing

--- neovim/nvim/lua/girol/init.lua
+++ neovim/nvim/lua/girol/init.lua
@@ -6,14 +6,14 @@ local current_theme = "tokyonight"
 -- Configures Lazy Plugin Manager
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 

c3a59893ccc19fe1717d25218008847ae43987e0
André Luiz Girol <andregirol@gmail.com>
2024-05-16 18:29:45 -0300

    Configures theme

--- neovim/nvim/lua/girol/init.lua
+++ neovim/nvim/lua/girol/init.lua
@@ -1,1 +1,23 @@
 require("girol/set")
+
+local plugins_path = "girol.plugins"
+local current_theme = "tokyonight"
+
+-- Configures Lazy Plugin Manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup(plugins_path, opts)
+-- END Lazy
+
+vim.cmd.colorscheme(current_theme)

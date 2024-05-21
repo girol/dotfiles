2b4fa7225158991b487aca0a3a57e0d451119af7
André Luiz Girol <andregirol@gmail.com>
2024-05-20 23:25:14 -0300

    Adds more customization to runtime and commands

--- neovim/nvim/lua/girol/set.lua
+++ neovim/nvim/lua/girol/set.lua
@@ -1,11 +1,22 @@
--- Configuration related to all vim "set" commands
+-- Customization related to all vim defaults: commands, behavior and runtime
+
+-- General setup
 vim.g.mapleader = " "
+
+-- enables mouse
+vim.opt.mouse = 'a'
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
 
 -- Dev Experience
 vim.opt.guicursor = ""
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "88"
+-- Don't show the mode, since it's already in the status line
+vim.opt.showmode = false
 
 -- Tabs and Spaces
 vim.opt.tabstop = 4
@@ -14,7 +25,7 @@ vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
 -- Search behavior
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
 
 -- Runtime

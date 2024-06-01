9751e6f7d6601cbb396c64c8c1f1e57302eaeec4
André Luiz Girol <andregirol@gmail.com>
2024-06-01 17:36:04 -0300

    refactor: nvim telescope
    - Moves keymaps to personal file
    - Adds reference to future deep dive

--- neovim/nvim/lua/girol/keymaps.lua
+++ neovim/nvim/lua/girol/keymaps.lua
@@ -1,2 +1,13 @@
 -- Remaps leader key just in case?
 vim.g.mapleader = " "
+
+local keymap = vim.keymap
+
+-- Telescope
+local builtin = require('telescope.builtin')
+
+-- Default mappings suggested by documentation
+keymap.set('n', '<leader>ff', builtin.find_files, {})
+keymap.set('n', '<leader>fg', builtin.live_grep, {})
+keymap.set('n', '<leader>fb', builtin.buffers, {})
+keymap.set('n', '<leader>fh', builtin.help_tags, {})

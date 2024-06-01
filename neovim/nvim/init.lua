a44784ee2f59aedf10b4bec22f94b1130f386e58
André Luiz Girol <andregirol@gmail.com>
2024-06-01 16:24:55 -0300

    Moves plugins into the default expected path
    Just keeping things simple

--- neovim/nvim/init.lua
+++ neovim/nvim/init.lua
@@ -1,3 +1,36 @@
--- Kept here as a reminder to initialize the personal module in case of
--- using this configuration with some neovim distribution like NvChad
-require("girol")
+--[[
+    # DISCLAIMER #
+
+    After studying a bit, I found out that Lazy is the "de facto" plugin manager
+for the most setups I've found.
+    Therefore, there is a certain pattern of configuration among all setups.
+    So, to keep things less confusing, hard to compare and maintain, I chose this
+folder structure to keep the configuration clean and reproductable
+
+Folder structure was taken from nvim-starter-kit from bcampolo in github
+
+]]--
+
+-- Configures Lazy Plugin Manager
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- This has to be set before initializing lazy
+vim.g.mapleader = " "
+
+require("lazy").setup("plugins", opts)
+-- END Lazy
+
+-- Personal configuratio are NOT loaded by lazy
+-- require("core.options")
+-- require("core.keymaps")

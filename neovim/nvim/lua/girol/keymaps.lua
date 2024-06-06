a5615cae081ba788fae72d1f7b6c3762965f958b
André Luiz Girol <andregirol@gmail.com>
2024-06-06 20:17:53 -0300

    Adds nvim-tree shortcuts testing them

--- neovim/nvim/lua/girol/keymaps.lua
+++ neovim/nvim/lua/girol/keymaps.lua
@@ -11,3 +11,7 @@ keymap.set('n', '<leader>ff', builtin.find_files, {})
 keymap.set('n', '<leader>fg', builtin.live_grep, {})
 keymap.set('n', '<leader>fb', builtin.buffers, {})
 keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- nvim-tree
+keymap.set('n', '<leader>ee', ":NvimTreeToggle<CR>") -- toggle file explorer
+keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>") -- find file in file explorer

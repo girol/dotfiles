828e187433856d618d5789a0d9eeb8e5352de6bb
André Luiz Girol <andregirol@gmail.com>
2024-06-23 15:42:57 -0300

    Refactor keymaps and adds one for file formatting

--- neovim/nvim/lua/girol/keymaps.lua
+++ neovim/nvim/lua/girol/keymaps.lua
@@ -3,15 +3,26 @@ vim.g.mapleader = " "
 
 local keymap = vim.keymap
 
--- Telescope
-local builtin = require('telescope.builtin')
+-- Imports for Plugin related keymaps
+local telescope = require("telescope.builtin")
+local conform = require("conform")
 
+-- Telescope
 -- Default mappings suggested by documentation
-keymap.set('n', '<leader>ff', builtin.find_files, {})
-keymap.set('n', '<leader>fg', builtin.live_grep, {})
-keymap.set('n', '<leader>fb', builtin.buffers, {})
-keymap.set('n', '<leader>fh', builtin.help_tags, {})
+keymap.set("n", "<leader>ff", telescope.find_files, {})
+keymap.set("n", "<leader>fg", telescope.live_grep, {})
+keymap.set("n", "<leader>fb", telescope.buffers, {})
+keymap.set("n", "<leader>fh", telescope.help_tags, {})
 
 -- nvim-tree
-keymap.set('n', '<leader>ee', ":NvimTreeToggle<CR>") -- toggle file explorer
+keymap.set("n", "<leader>ee", ":NvimTreeToggle<CR>") -- toggle file explorer
 keymap.set("n", "<leader>ef", ":NvimTreeFindFile<CR>") -- find file in file explorer
+
+-- conform - formatting plugin
+keymap.set({ "n", "v" }, "<leader>ft", function()
+	conform.format({
+		lsp_fallback = true,
+		async = false,
+		timeout_ms = 1000,
+	})
+end, { desc = "Format file. Format range (Visual)" })
